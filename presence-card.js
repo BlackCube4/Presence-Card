14c4c7c6577d33a490ab006a01c20a318530646e
BlackCube <102918591+BlackCube4@users.noreply.github.com>
2024-02-14 07:09:23 +0100

    Add Color

--- presence-card.js
+++ presence-card.js
@@ -49,6 +49,17 @@ class PresenceCard extends HTMLElement {
             
             //console.log(image.naturalHeight, image.naturalWidth);
 
+            if (this.textScale == null)
+                this.textScale = 1.5;
+            if (this.zone1C == null)
+                this.zone1C = "#a9521b";
+            if (this.zone2C == null)
+                this.zone2C = "#218924";
+            if (this.zone3C == null)
+                this.zone3C = "#4f2a81";
+            if (this.zone4C == null)
+                this.zone4C = "#1b3ea9";
+
             this.innerHTML = `
                 <style>
                     #presenceCardContainer {
@@ -72,7 +83,7 @@ class PresenceCard extends HTMLElement {
                         overflow: visible;
                         width: 1px; 
                         height: 1px;
-                        transform: scale(4);
+                        transform: scale(1.5);
                     }
                     .target {
                         position: absolute;
@@ -94,8 +105,8 @@ class PresenceCard extends HTMLElement {
                         display: flex
                     }
                     #zone1 {
-                        border-color: #a9521b;
-                        background: #89602121;
+                        border-color: ${this.zone1C};
+                        background: ${this.zone1C + "21"};
                         height: ${this.zone1sx != null ? (this._hass.states[this.zone1ex].state - this._hass.states[this.zone1sx].state) / this.roomX / 10 : 0}%;
                         width: ${this.zone1sx != null ? (this._hass.states[this.zone1ey].state - this._hass.states[this.zone1sy].state) / this.roomY / 10 : 0}%;
                         top: ${this.zone1sx != null ? this.invertX == false ? this._hass.states[this.zone1sx].state / this.roomX / 10 : -this._hass.states[this.zone1ex].state / this.roomX / 10 : 0}%;
@@ -103,8 +114,8 @@ class PresenceCard extends HTMLElement {
                         display: ${this.zone1sx != null ? "flex" : "none"};
                     }
                     #zone2 {
-                        border-color: #218924;
-                        background: #21892421;
+                        border-color: ${this.zone2C};
+                        background: ${this.zone2C + "21"};
                         height: ${this.zone2sx != null ? (this._hass.states[this.zone2ex].state - this._hass.states[this.zone2sx].state) / this.roomX / 10 : 0}%;
                         width: ${this.zone2sx != null ? (this._hass.states[this.zone2ey].state - this._hass.states[this.zone2sy].state) / this.roomY / 10 : 0}%;
                         top: ${this.zone2sx != null ? this.invertX == false ? this._hass.states[this.zone2sx].state / this.roomX / 10 : -this._hass.states[this.zone2ex].state / this.roomX / 10 : 0}%;
@@ -112,8 +123,8 @@ class PresenceCard extends HTMLElement {
                         display: ${this.zone2sx != null ? "flex" : "none"};
                     }
                     #zone3 {
-                        border-color: #4f2a81;
-                        background: #7a218921;
+                        border-color: ${this.zone3C};
+                        background: ${this.zone3C + "21"};
                         height: ${this.zone3sx != null ? (this._hass.states[this.zone3ex].state - this._hass.states[this.zone3sx].state) / this.roomX / 10 : 0}%;
                         width: ${this.zone3sx != null ? (this._hass.states[this.zone3ey].state - this._hass.states[this.zone3sy].state) / this.roomY / 10 : 0}%;
                         top: ${this.zone3sx != null ? this.invertX == false ? this._hass.states[this.zone3sx].state / this.roomX / 10 : -this._hass.states[this.zone3ex].state / this.roomX / 10 : 0}%;
@@ -121,8 +132,8 @@ class PresenceCard extends HTMLElement {
                         display: ${this.zone3sx != null ? "flex" : "none"};
                     }
                     #zone4 {
-                        border-color: #1b3ea9;
-                        background: #21458921;
+                        border-color: ${this.zone4C};
+                        background: ${this.zone4C + "21"};
                         height: ${this.zone4sx != null ? (this._hass.states[this.zone4ex].state - this._hass.states[this.zone4sx].state) / this.roomX / 10 : 0}%;
                         width: ${this.zone4sx != null ? (this._hass.states[this.zone4ey].state - this._hass.states[this.zone4sy].state) / this.roomY / 10 : 0}%;
                         top: ${this.zone4sx != null ? this.invertX == false ? this._hass.states[this.zone4sx].state / this.roomX / 10 : -this._hass.states[this.zone4ex].state / this.roomX / 10 : 0}%;
@@ -133,29 +144,59 @@ class PresenceCard extends HTMLElement {
                 <ha-card id="presenceCardContainer">
                     <div id="sensorContainer">
                         <div class="zone" id="zone1">
-                            <div style="transform: rotate(${-this.sensorRot}deg) scale(1.5);">Zone 1</div>
+                            <div style="transform: rotate(${-this.sensorRot}deg) scale(${this.textScale});">Zone 1</div>
                         </div>
                         <div class="zone" id="zone2">
-                            <div style="transform: rotate(${-this.sensorRot}deg) scale(1.5);">Zone 2</div>
+                            <div style="transform: rotate(${-this.sensorRot}deg) scale(${this.textScale});">Zone 2</div>
                         </div>
                         <div class="zone" id="zone3">
-                            <div style="transform: rotate(${-this.sensorRot}deg) scale(1.5);">Zone 3</div>
+                            <div style="transform: rotate(${-this.sensorRot}deg) scale(${this.textScale});">Zone 3</div>
                         </div>
                         <div class="zone" id="zone4">
-                            <div style="transform: rotate(${-this.sensorRot}deg) scale(1.5);">Zone 4</div>
+                            <div style="transform: rotate(${-this.sensorRot}deg) scale(${this.textScale});">Zone 4</div>
                         </div>
                         <svg id="orientation">
-                            <path id="path870" style="fill:#329420;fill-opacity:0.462745;stroke:#ebebeb;stroke-width:1;stroke-linecap:round;stroke-linejoin:round"
-                            d="M 7.884412,-14.710865 A 16.987328,16.987328 0 0 1 16.378076,5.9219e-4 16.987328,16.987328 0 0 1 7.884412,14.712049 L -0.60925159,5.9219e-4 Z" />
-                            <path id="path846" style="fill:#ebebeb;stroke:#ebebeb;stroke-width:1;stroke-linecap:round;stroke-linejoin:round"
-                            d="M 1.398982,2.6298402 C -3.678719,5.5614521 -3.15501,5.8638161 -3.1550097,5.9219e-4 c 3e-7,-5.86322399 -0.5237088,-5.56086099 4.5539919,-2.62924799 5.0777008,2.93161199 5.0777008,2.32688499 -2e-7,5.258496 z" />
+                            <path d="M 19.232715,-37.111366 1.9488856,-7.175191 c 11.7452154,7.17366839 11.7452154,7.17366839 0,14.3473368 L 19.232715,37.113366 C 32.492883,29.458258 40.66109,15.309703 40.660081,-0.00152261 40.659324,-15.310982 32.49137,-29.457267 19.232715,-37.111366 Z"
+                            style="fill:#329420;fill-opacity:0.462745;stroke:var(--primary-text-color);stroke-width:4.46249;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none"
+                            id="layer1" />
+                            <path d="M 2.8724973,6.6323504 C -9.9365527,14.027655 -8.6154373,14.790416 -8.6154373,-2.1289547e-4 -8.6154373,-14.790828 -9.9365527,-14.028092 2.8724973,-6.6327872 c 12.8090497,7.39530978 12.8090497,5.86981264 0,13.2651376 z"
+                            style="fill:#2d831b;fill-opacity:1;stroke:var(--primary-text-color);stroke-width:4.46249;stroke-linecap:round;stroke-linejoin:round"
+                            id="layer1-7" />
                         </svg>
-                        <div class="target" id="target1" style="background-color: #d38b63;"></div>
-                        <div class="target" id="target2" style="background-color: #75b148;"></div>
+                        <div class="target" id="target1" style="background-color: var(--primary-color);"></div>
+                        <div class="target" id="target2" style="background-color: var(--accent-color);"></div>
                         <div class="target" id="target3" style="background-color: #7548b1;"></div>
                     </div>
                 </ha-card>
             `;
+            
+            if (this.target1C != null) {
+                this.target = this.querySelector("#target1");
+                this.target.style.backgroundColor = this.target1C;
+            }
+            if (this.target2C != null) {
+                this.target = this.querySelector("#target2");
+                this.target.style.backgroundColor = this.target2C;
+            }
+            if (this.target2C != null) {
+                this.target = this.querySelector("#target3");
+                this.target.style.backgroundColor = this.target3C;
+            }
+            if (this.targetOutline != null) {
+                this.target = this.querySelector(".target");
+                this.target.style.borderColor = this.targetOutline;
+            }
+            
+            if (this.displaySensor == false) {
+                this.sensor = this.querySelector("#orientation");
+                this.sensor.style.display = "none";
+            }
+            if (this.displayText == false) {
+                this.texts = this.querySelectorAll(".zone div");
+                this.texts.forEach(text => {
+                    text.style.display = "none";
+                });
+            }
             
             this.invertX == false ? this.invertX = 1 : this.invertX = -1
             const cardElement = this.querySelector('#presenceCardContainer');
@@ -196,25 +237,21 @@ class PresenceCard extends HTMLElement {
         this.zone1ex = config.zone1ex;
         this.zone1sy = config.zone1sy;
         this.zone1ey = config.zone1ey;
-        this.zone1o = config.zone1o;
 
         this.zone2sx = config.zone2sx;
         this.zone2ex = config.zone2ex;
         this.zone2sy = config.zone2sy;
         this.zone2ey = config.zone2ey;
-        this.zone2o = config.zone2o;
 
         this.zone3sx = config.zone3sx;
         this.zone3ex = config.zone3ex;
         this.zone3sy = config.zone3sy;
         this.zone3ey = config.zone3ey;
-        this.zone3o = config.zone3o;
 
         this.zone4sx = config.zone4sx;
         this.zone4ex = config.zone4ex;
         this.zone4sy = config.zone4sy;
         this.zone4ey = config.zone4ey;
-        this.zone4o = config.zone4o;
 
         this.imageLoc = config.image;
         this.roomX = config.roomX;
@@ -222,6 +259,20 @@ class PresenceCard extends HTMLElement {
         this.sensorX = config.sensorX;
         this.sensorY = config.sensorY;
         this.sensorRot = config.sensorRot;
+
+        this.target1C = config.target1Color;
+        this.target2C = config.target2Color;
+        this.target3C = config.targe31Color;
+        this.targetOutline = config.targetOutline;
+
+        this.zone1C = config.zone1Color;
+        this.zone2C = config.zone2Color;
+        this.zone3C = config.zone3Color;
+        this.zone4C = config.zone4Color;
+
+        this.displaySensor = config.displaySensor;
+        this.displayText = config.displayText;
+        this.textScale = config.textScale;
 
         this.connectedCallback()
     }
@@ -240,22 +291,18 @@ class PresenceCard extends HTMLElement {
             zone1ex: 'number.everything_presence_lite_zone_1_end_x',
             zone1sy: 'number.everything_presence_lite_zone_1_begin_y',
             zone1ey: 'number.everything_presence_lite_zone_1_end_y',
-            zone1o: 'binary_sensor.everything_presence_lite_zone_1_occupancy',
             zone2sx: 'number.everything_presence_lite_zone_2_begin_x',
             zone2ex: 'number.everything_presence_lite_zone_2_end_x',
             zone2sy: 'number.everything_presence_lite_zone_2_begin_y',
             zone2ey: 'number.everything_presence_lite_zone_2_end_y',
-            zone2o: 'binary_sensor.everything_presence_lite_zone_2_occupancy',
             zone3sx: 'number.everything_presence_lite_zone_3_begin_x',
             zone3ex: 'number.everything_presence_lite_zone_3_end_x',
             zone3sy: 'number.everything_presence_lite_zone_3_begin_y',
             zone3ey: 'number.everything_presence_lite_zone_3_end_y',
-            zone3o: 'binary_sensor.everything_presence_lite_zone_3_occupancy',
             zone4sx: 'number.everything_presence_lite_zone_4_begin_x',
             zone4ex: 'number.everything_presence_lite_zone_4_end_x',
             zone4sy: 'number.everything_presence_lite_zone_4_begin_y',
             zone4ey: 'number.everything_presence_lite_zone_4_end_y',
-            zone4o: 'binary_sensor.everything_presence_lite_zone_4_occupancy',
             image: '/local/images/room.jpg',
             roomX: 3.84,
             roomY: 4.81,
